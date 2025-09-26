16ebf6864a6aeb859f5d1681bd7f4d561ce084ba
Tanja <lucia70374@gmail.com>
2025-09-26 14:18:56 +0200

    added search by filters

--- features/search.feature
+++ features/search.feature
@@ -1,26 +1,20 @@
-# Create Tests using Gherkin syntax
-
 Feature: Search Functionality
 
-  Scenario: Successful search with valid input
+  Background:
     Given the user is on the homepage
-    When the user enters "semrush ai" in the search bar and clicks enter
-    Then the search results for "semrush ai" should be displayed
 
-  #Scenario: Search with mixed case input
-  #  Given the user is on the homepage
-  #  When the user enters "SeMrUsH Ai" in the search bar
-  #  When the user clicks the search button
-  #  Then the search results for "semrush ai" should be displayed
+  Scenario Outline: Successful search with valid input
+    When the user enters "<searchTerm>" in the search bar and clicks enter
+    Then the search results for "<searchTerm>" should be displayed
+    When the user clicks filter button "<filterName>" that button should be highlighted
+    Examples:
+      | searchTerm | filterName |
+      | semrush ai | images |
+      | seeemrush aai | news   |
+      | SEmRuSh aI | videos |
 
   #Scenario: Search with empty input
   #  Given the user is on the homepage
   #  When the user leaves the search bar empty
   #  When the user clicks the search button
   #  Then a message prompting to enter a search term should be displayed
-
-  #Scenario: Search with leading and trailing spaces
-  #  Given the user is on the homepage
-  #  When the user enters "  prowly ai tools  " in the search bar
-  #  When the user clicks the search button
-  #  Then the search results for "prowly ai tools" should be displayed
--- features/step_definitions/search.steps.ts
+++ features/step_definitions/search.steps.ts
@@ -8,12 +8,52 @@ Given('the user is on the homepage', async function (this: CustomWorld) {
 
 When('the user enters {string} in the search bar and clicks enter', async function (this: CustomWorld, searchTerm: string) {
   const searchBar = this.page.getByTestId('search-form-input');
-  await searchBar.fill(searchTerm);
+  switch (searchTerm){
+    case "semrush ai": 
+    await searchBar.fill("semrush ai");
+    break;
+    case "seeemrush aai":
+    await searchBar.fill("seeemrush aai");
+    break;
+    case "SEmRuSh aI":
+    await searchBar.fill("SEmRuSh aI");
+  }
   await this.page.keyboard.press('Enter');
 });
 
 Then('the search results for {string} should be displayed', async function (this: CustomWorld, searchTerm: string) {
-    const header = this.page.getByTestId('related-queries-header');
+    const header = this.page.getByTestId('related-queries-header').first();
     await header.waitFor({ state: 'visible' });
-    await expect(header).toContainText(searchTerm);
+    switch (searchTerm){
+    case "semrush ai": 
+    await expect(header).toContainText("semrush ai");
+    break;
+    case "seeemrush aai":
+    await expect(header).toContainText("seeemrush aai");
+    break;
+    case "SEmRuSh aI":
+    await expect(header).toContainText("SEmRuSh aI");
+    }
+});
+
+When('the user clicks filter button {string} that button should be highlighted', { timeout: 10 * 1000 }, async function (this: CustomWorld, filterName: string) {
+    const filterBtn = this.page.getByLabel('Search menu').first();
+    switch (filterName){
+        case "images":
+        const imagesTab = filterBtn.locator('a[href^="/images"]');
+        await imagesTab.click();
+        await this.page.waitForLoadState('networkidle');
+        await expect(imagesTab).toHaveClass('tab tab--highlighted tab--highlight-bar tab--icon');
+        break;
+        case "news":
+        const newsTab = filterBtn.locator('a[href^="/news"]');
+        await newsTab.click();
+        await this.page.waitForLoadState('networkidle');
+        await expect(newsTab).toHaveClass('tab tab--highlighted tab--highlight-bar tab--icon');
+        case "videos":
+        const videosTab = filterBtn.locator('a[href^="/videos"]');
+        await videosTab.click();
+        await this.page.waitForLoadState('networkidle');
+        await expect(videosTab).toHaveClass('tab tab--highlighted tab--highlight-bar tab--icon');
+    }
 });
