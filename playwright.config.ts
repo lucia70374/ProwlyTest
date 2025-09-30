c0c297298d6b588ee108ead3f865804de9757e5e
Tanja <lucia70374@gmail.com>
2025-09-30 21:17:39 +0200

    trace

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,3 +1,9 @@
 import { defineConfig } from '@playwright/test';
 
-export default defineConfig({});
+export default defineConfig({
+    retries: process.env.CI ? 1 : 0, // Retry once on CI
+  use: {
+    trace: 'on-first-retry',
+    video: 'on-first-retry',
+  },
+});
